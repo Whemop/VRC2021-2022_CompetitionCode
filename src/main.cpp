f9115d93945b28d959ead1a335d6770dc5547f00
Whemop <94915774+Whemop@users.noreply.github.com>
2021-12-09 14:46:30 -0500

    v3.1.3.1
    EXPRIMENTAL: Added code that should make the front loader goto set 
    positions when button is held instead of just setting velocity.

--- src/main.cpp
+++ src/main.cpp
@@ -288,13 +288,13 @@ void opcontrol() {
 
 		//Grabber Motor Control
 		if (master.get_digital(DIGITAL_L1)) {
-			front_loader.move_velocity(200);
+			front_loader.move_relative(2000, 100);
 		}
 		else if (master.get_digital(DIGITAL_L2)) {
-			front_loader.move_velocity(-200);
+			front_loader.move_relative(-2000, 100);
 		}
 		else {
-			front_loader.move_velocity(0);
+			front_loader.move_absolute(0, 100);
 		}
 
 		//Ring intake Motor Controller
